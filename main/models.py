772dd95a7830e5e6d6af2dcc8c74ab1fb00247e1
Jessie Eoff <jessieeoff2@gmail.com>
2020-02-19 06:57:15 -0500

    Initial additions to models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Tutor(models.Model):
+    #from google API
+    tutor_ID = models.CharField(max_length = 100) #not sure how long these IDs are
+
+
+class Tutee(models.Model):
+    #from google API
+    tutee_ID = models.CharField(max_length = 100)
+
+class Profile(models.Model):
+    #change to true when the profile is created ?
+    existing_profile = models.BooleanField(default = False)
+
+    #from the google API
+    new_user_ID = models.CharField(max_length = 100)
+    new_user_email = models.CharField(max_length = 50)
+    new_user_image = models.CharField()
+
+    #additional information pulled from a new user profile
+    date_created = models.DateTimeField(default = timezone.now)
+
