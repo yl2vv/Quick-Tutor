0991afff5549694ac72b965014f38f2485752d1e
James <yl2vv@virginia.edu>
2020-02-23 23:44:56 -0500

    Created models and display results (no images yet)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    Question_text = models.CharField(max_length=200)
+    Class_text = models.CharField(max_length=200)
+    Comments_text = models.CharField(max_length=10000)
+    File_upload = models.ImageField()
+    def __str__(self):
+        return self.Question_text
--- main/views.py
+++ main/views.py
@@ -3,10 +3,32 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Question
 
 
 def question(request):
-    return render(request, 'main/question.html')
+    #Add data to models
+    print("hello")
+    if request.method == "POST":
+        #Get the user inputs
+        question = request.POST.get('Question')
+        class_id = request.POST.get('class')
+        file_upload = request.POST.get('upload')
+        comments = request.POST.get('comments')
+        print(question)
+        #Store in model
+        obj = Question() 
+        obj.Question_text = question
+        obj.Class_text = class_id
+        obj.File_upload = file_upload
+        obj.Comments_text = comments
+        obj.save()
+    context = {}
+    return render(request, 'main/question.html', context)
 
 def results(request):
-    return render(request, 'main/results.html')
+    queryset = Question.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, 'main/results.html', context)
