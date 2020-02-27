69ca6a945865591e142b29081f8c9247f1d47270
Andrew Downing <awd4td@virginia.edu>
2020-02-26 21:49:12 -0500

    should be ready for heroku

--- QuickTutor/settings.py
+++ QuickTutor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,6 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = "/loggedIn"
 
 #AUTH_USER_MODEL = 'main.CustomUser'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
