6266a98503ea6c9346ec04ab00bbc5decf17512a
Andrew Downing <awd4td@virginia.edu>
2020-03-01 16:26:19 -0500

    i think this will be the one

--- QuickTutor/settings.py
+++ QuickTutor/settings.py
@@ -87,17 +87,28 @@ WSGI_APPLICATION = 'QuickTutor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quicktutor4',
-        'USER': 'groupuser',
-        'PASSWORD': 'tutoring!',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+if 'HEROKU' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd6humjm2sv80ba',
+            'USER': 'bonxsspvtuuons',
+            'PASSWORD': '2c88c692268646249453ec7448aed5dbc5c3ce859738794d5a9e86abd004fd1d',
+            'HOST': 'ec2-3-234-169-147.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'quicktutor4',
+            'USER': 'groupuser',
+            'PASSWORD': 'tutoring!',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 # install postgres on this machine
 # you'll enter root user credentials in the process
 # use the credentials to create another user (this should be the same for all your group memebers)
