42471b982436f764b009018b35c264237ba2c1e1
Andrew Downing <awd4td@virginia.edu>
2020-03-02 14:48:43 -0500

    try again

--- QuickTutor/settings.py
+++ QuickTutor/settings.py
@@ -94,12 +94,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'QuickTutor.wsgi.application'
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 #Database
 #https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 if 'HEROKU' in os.environ:
