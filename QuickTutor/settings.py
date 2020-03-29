d65db7c25a869cf5b0ee147cd333469f14cebcea
Mykim <mdl8dg@virginia.edu>
2020-03-28 23:51:28 -0400

    added tutor page

--- QuickTutor/settings.py
+++ QuickTutor/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'QuickTutor.wsgi.application'
     }
 }'''
 
-DATABASES = {'default': dj_database_url.parse('postgres://postgres:Keshav00@localhost:5432/quicktutor')}
+DATABASES = {'default': dj_database_url.parse('postgres://postgres:hello@localhost:5432/quicktutor')}
 
 # DATABASES = {
 #     'default': {
